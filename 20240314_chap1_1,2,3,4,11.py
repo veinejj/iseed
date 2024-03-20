895c28ada9e934068b3ead08927f8931a9a2aae9
veinejj <162970418+veinejj@users.noreply.github.com>
2024-03-20 16:41:37 +0900

    Create 20240314_chap1_1,2,3,4,11.py

---
+++ 20240314_chap1_1,2,3,4,11.py
@@ -0,0 +1,30 @@
+###1번문제###  답 : 4
+print("100")
+
+print(100)
+
+print(50+50)
+
+print("50+50")
+
+###2번문제###  답 : 3
+print('%d / %d = %d' % (5, 10, 5/10))
+
+###3번문제###  답 : 0876, CookBook, 123.5
+print("%04d"%876)
+
+print("%5s"%"CookBook")
+
+print("%1.1f"%123.45)
+
+###4번문제###  답 : 333111222
+print("{2:d}{0:d}{1:d}".format(111, 222, 333))
+
+###11번문제###  답 : 11, 26
+jinsu_2 = "1011"
+two_jinsu = int(jinsu_2, 2)
+print(two_jinsu)
+
+jinsu_16 = "1A"
+sixteen_jinsu = int(jinsu_16, 16)
+print(sixteen_jinsu)
